0851aefccc43ffe1c17a1cd174a61894c4e05636
cogo79 <migobigo76@gmail.com>
2016-04-18 17:45:39 +0200

    Removed mongodb

--- server.js
+++ server.js
@@ -1,40 +1,18 @@
 var express = require('express'),
-	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+	bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
-
-function compile(str, path) {
-	return stylus(str).set('filename', path);
-}
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(stylus.middleware ({
-	src: __dirname + '/public',
-	compile: compile
-}));
-app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/theclubb');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-	console.log('theclubb db opened');
-});
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-	mongoMessage = messageDoc.message;
-});
+app.use(express.static(__dirname + '/public'));
 
 app.get('/partials/:partialPath', function(req, res) {
 	res.render('partials/' + req.params.partialPath);
@@ -42,7 +20,7 @@ app.get('/partials/:partialPath', function(req, res) {
 
 app.get('*', function(req, res) {
 	res.render('index', {
-		mongoMessage: mongoMessage
+
 	});
 });
 
